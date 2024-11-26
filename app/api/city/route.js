5f7295157068f327dedfdf3b1583e955d520251b
arya0509 <151792793+arya0509@users.noreply.github.com>
2024-11-26 16:52:13 -0700

    Initial commit, Added API's

---
+++ app/api/city/route.js
@@ -0,0 +1,23 @@
+ 
+const apiKey=process.env.ACCUWEATHER_API_KEY;
+
+export async function GET(req,res) {
+    try{
+        
+        const url=new URL(req.url);
+        const q=url.searchParams.get("q");
+        
+        const response= await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${q}`);
+        if(response.ok){
+            const data=await response.json();
+           
+            return new Response(JSON.stringify(data),{status:response.status});
+        }
+        else{
+        return new Response(JSON.stringify({message:"Could not retrieve the cities. Please try again later"}),{status:response.status})
+        }
+    }catch(error){
+        console.log(`Error message: ${error}`)
+        return new Response(JSON.stringify("error",{status:404}))
+    }
+}
