c43832c236f92169ffa810c109529d24d2840c9f
arya0509 <151792793+arya0509@users.noreply.github.com>
2024-12-02 16:16:43 -0700

    fixing the apostrophe error

--- app/components/todaysWeather.js
+++ app/components/todaysWeather.js
@@ -4,7 +4,7 @@ export default function TodaysWeather() {
     const hourlyTimes = ['12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00','00:00','01:00','02:00','03:00'];
     return(
         <div className="bg-slate-800 w-[90%] h-48 overflow-hidden overflow-x-visible ">
-            <h2 className="text-white font-extrabold m-3 mt-0">Today's Weather</h2>
+            <h2 className="text-white font-extrabold m-3 mt-0">Today&apos;s Weather</h2>
             <div className="flex flex-row mt-5">
                 {
                     hourlyTemps.map((temp,index) => {
